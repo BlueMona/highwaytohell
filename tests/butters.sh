224441af2f7878a4ecbe25cacba7217d3aa04c09
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-08-15 11:13:08 +0200

    refactor(tests): moar & don't stop on first failure

--- tests/butters.sh
+++ tests/butters.sh
@@ -1,21 +1,47 @@
 #!/bin/sh
 
+ret=0
 if ! ./samples.d/butters | grep peerio.com; then
     echo failed listing domains
-    exit 1
-#elif ! ./samples.d/butters -d peerio.com -a get --getdsrecords | grep '^{}$'; then
+    ret=`expr ret + 1`
+fi
+#if ! ./samples.d/butters -d peerio.com -a get --getdsrecords | grep '^{}$'; then
 # backends error on circleci? should divert logs to file
 #    echo failed fetching ds records
-#    exit 1
-elif ! ./samples.d/butters -d peerio.com -a get --getdsrecords | grep '^{}$'; then
+#    ret=`expr ret + 1`
+#fi
+if ! ./samples.d/butters -d peerio.com -a get --getdsrecords | grep '^{}$'; then
     echo failed fetching ds records
-    exit 1
-elif ! ./samples.d/butters -R notifications; then
+    ret=`expr ret + 1`
+fi
+if ! ./samples.d/butters -R notifications 2>&1 | grep 'authentication failed'; then
+    echo authenticated against default domain example.com, which should not exist nor belong to test user
+    ret=`expr ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R notifications | grep '^\[\]$'; then
     echo failed listing notifications
-    exit 1
-elif ! ./samples.d/butters -R records; then
+    ret=`expr ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R records | grep iceblobvirginia; then
     echo failed listing records
-    exit 1
+    ret=`expr ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R records -a add -r @ -T myrelay.mail.com -t MX --priority 10
+    echo failed adding mx record
+    ret=`expr ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R records -a get --record @ | grep mailrelay; then
+    echo failed fetching record by name
+    ret=`expr ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R records -a del -r @; then
+    echo failed dropping record
+    ret=`expr ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R healthchecks | grep peerio.com; then
+    echo failed listing health checks
+    ret=`expr ret + 1`
 fi
 
-exit 0
+echo ret is $ret
+exit $ret
